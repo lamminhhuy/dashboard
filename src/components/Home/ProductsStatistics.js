6b2c29fd845167c45c73cfcbbf628a926cbd21fb
lamminhhuy <lhuy00429@gmail.com>
2022-11-29 18:18:18 +0700

    make it better

--- src/components/Home/ProductsStatistics.js
+++ src/components/Home/ProductsStatistics.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProductsStatistics = () => {
+const ProductsStatisics = () => {
   return (
     <div className="col-xl-6 col-lg-12">
       <div className="card mb-4 shadow-sm">
